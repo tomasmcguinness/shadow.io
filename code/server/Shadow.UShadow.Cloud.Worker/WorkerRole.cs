3e31ba913952b954b8b0f17307e0c00aa0014c73
Tomas McGuinness <tomas@tomasmcguinness.com>
2012-10-28 12:18:51 +0000

    Connected up a simple Azure Worker role and integrated twitter streaming client!

---
+++ code/server/Shadow.UShadow.Cloud.Worker/WorkerRole.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Diagnostics;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using Microsoft.WindowsAzure.StorageClient;
+using Tweetinvi;
+using oAuthConnection;
+
+namespace Shadow.UShadow.Cloud.Worker
+{
+    public class WorkerRole : RoleEntryPoint
+    {
+        public override void Run()
+        {
+            // This is a sample worker implementation. Replace with your logic.
+            Trace.WriteLine("Shadow.UShadow.Cloud.Worker entry point called", "Information");
+
+            // Connect to Twitter.
+            //
+            Stream.ProcessTweetDelegate produceTweetDelegate = new Stream.ProcessTweetDelegate(processTweet);
+
+            // Creating the stream and specifying the delegate
+            Stream myStream = new Stream(produceTweetDelegate);
+
+            Token token = new Token()
+            {
+                AccessToken = "14910567-1AnQzrOuwPFsPd9HrcXrCPBmW7Cwf7doxXqd5vdgi",
+                AccessTokenSecret = "hHzkPdejgaJkt9VKxW7JGDC0AnJ5GzvlRjhNaHzYzW4",
+                ConsumerKey = "WFYdWcCCTZk4ASituGqhqQ",
+                ConsumerSecret = "DfOmNZOrhN31vuEDpm10lxkSSB1MfjVA4Y9oPzSK0gc"
+            };
+
+            myStream.StreamUrl = "https://userstream.twitter.com/1.1/user.json?with=user";
+
+            // Starting the stream by specifying credentials thanks to the Token
+            myStream.StartStream(token);
+
+            while (true)
+            {
+                Thread.Sleep(10000);
+                Trace.WriteLine("Working", "Information");
+            }
+        }
+
+        private void processTweet(Tweet tweet, bool force)
+        {
+            if (tweet != null)
+            {
+                Trace.WriteLine(tweet.text, "Information");
+            }
+        }
+
+        public override bool OnStart()
+        {
+            // Set the maximum number of concurrent connections 
+            ServicePointManager.DefaultConnectionLimit = 12;
+
+            // For information on handling configuration changes
+            // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
+
+            return base.OnStart();
+        }
+    }
+}
--- code/server/Shadow.UShadow/Controllers/ServicesController.cs
+++ code/server/Shadow.UShadow/Controllers/ServicesController.cs
@@ -8,13 +8,9 @@ namespace Shadow.UShadow.Controllers
 {
     public class ServicesController : Controller
     {
-        //
-        // GET: /Services/
-
         public ActionResult Index()
         {
             return View();
         }
-
     }
 }
